0f8a2c0f73018f9f44976ba3f2944a035fe430ce
Christian Koch Echeverria <s0566264@htw-berlin.de>
2026-02-25 15:19:43 +0100

    #1 created first part of the progress component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+import { Progress } from "@/components/ui/progress";
+import { Press_Start_2P } from "next/font/google";
+
+const pressStart2P = Press_Start_2P({
+    weight: "400",
+    subsets: ["cyrillic"],
+});
+
 export default function Home() {
+    //  TODO: Move to colors later
+    const GREY_AZURE = "#365b6d";
+    const CYAN = "#41c1ba";
+    const LIGHT_GREY = "#f7f7f7";
+
     return (
-        <div className="bg-background">
-            <h1 className="mt-10 text-center text-4xl font-bold">
-                Hello, world!
-            </h1>
+        <div
+            className={`bg-background flex h-screen w-full items-center justify-center ${pressStart2P.className} `}
+        >
+            <div className="flex w-full flex-col">
+                <p className="text-overlay-primary mb-5 text-5xl">
+                    Project Progress
+                </p>
+                <Progress
+                    value={50}
+                    className="h-12"
+                    indicatorColor={CYAN}
+                    backgroundColor={GREY_AZURE}
+                />
+            </div>
         </div>
     );
 }
